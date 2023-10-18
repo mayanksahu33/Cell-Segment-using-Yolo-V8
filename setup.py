b4993cfeb7e90ce0a45ae8d160cc485eab6a86bd
Mayank Sahu <sahumayank.18@email.com>
2023-10-18 22:37:29 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='CellSegmentation',
+    version='0.0.0',
+    author='Mayank Sahu',
+    author_email='sahumayank.18@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
